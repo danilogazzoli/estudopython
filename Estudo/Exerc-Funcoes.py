1821512ae3069351ec711ab3ad53a18167490ee8
danilogazzoli <daniloresende@yahoo.com>
2019-10-11 22:58:29 -0300

    funções

--- Estudo/Exerc-Funcoes.py
+++ Estudo/Exerc-Funcoes.py
@@ -7,6 +7,11 @@ def calculadoraVariosRetornos(x,y):
 def velocidade_media(distancia, tempo):
     return distancia / tempo
 
+def teste(arg, *args):
+    print('O primeiro argumento é: {}'.format(arg))
+    for arg2 in args:
+        print('os outros são: {}'.format(arg2))
+
 ## main
 print(calculadora(4,5))
 print(type(calculadora(4,5)))
@@ -15,4 +20,9 @@ print(calculadoraVariosRetornos(10, 30))
 print(type(calculadoraVariosRetornos(10, 30)))
 
 print('Velocidade {}'.format(velocidade_media(100, 10)))
+
+teste('este é um teste', 1, 2, 3, 'vários argumentos')
+
+lista = ["a", 'b', 'c']
+teste('param1', *lista)
 
